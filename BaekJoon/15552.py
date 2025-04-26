9101459232096df0a5ed7eceb58e4bb203bd247c
Jushin-Hwang <wntls0610@gmail.com>
2025-04-26 17:45:29 +0900

    feat: 백준 15552번 문제 풀이 추가

---
+++ BaekJoon/15552.py
@@ -0,0 +1,18 @@
+# 2025년 4월 26일
+# 백준 15552번
+import sys
+
+def get_number() :
+    a = sys.stdin.readline().rstrip()
+    a_int = int(a)
+    return a_int
+
+def get_numbers() :
+    a = sys.stdin.readline().rstrip()
+    a_list = [int(number) for number in a.split(' ')]    
+    return a_list
+
+T = get_number()
+for i in range(T) :
+    number_list = get_numbers()
+    print(number_list[0] + number_list[1])
